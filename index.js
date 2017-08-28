0e25dfe3eae252b0169dc130688796fc46b5d46b
Garrett McCullough <garrett.mccullough@build.com>
2017-08-27 20:15:44 -0700

    convert into the webtask format and add a wrapper so you can still run from the terminal

--- index.js
+++ index.js
@@ -1,70 +1,28 @@
-const rp = require('request-promise-native');
 const argv = require('minimist')(process.argv.slice(2));
-const moment = require('moment');
-require('moment-recur');
-
-const API_URL = 'https://api.github.com/';
-const GITHUB_RAW_URL = 'https://raw.githubusercontent.com/';
+const webtask = require('./webtask');
 
 const {
-	o: owner,
-	r: repo,
-	a: access_token
+	o,
+	r,
+	d,
+	w,
+	g: github_token
 } = argv;
-
-if (!owner || !repo || !access_token) {
-	console.log('Usage: \nnode index.js -a GITHUB_API_TOKEN -o REPO_OWNER_NAME -r REPO_NAME');
-	return;
-}
-
-const defaultOptions = {
-	qs: {
-		access_token
+const context = {
+	data: {
+		o,
+		r,
+		d,
+		w,
 	},
-	headers: {
-		'User-Agent': 'Github Issue Webtask'
-	},
-	json: true
+	secrets: {
+		github_token,
+	}
 };
+webtask(context, (err, msg) => {
+	if (err) {
+		return console.error(err);
+	}
 
-const templateOptions = {
-	method: 'GET',
-	uri: `${GITHUB_RAW_URL}${owner}/${repo}/master/ISSUE_TEMPLATE.md`,
-};
-rp(Object.assign({}, defaultOptions, templateOptions))
-	.then(function (issueTemplate) {
-		const title = generateIssueTitle();
-		const issueOptions = {
-			method: 'POST',
-			uri: `${API_URL}repos/${owner}/${repo}/issues`,
-			body: {
-				title,
-				body: issueTemplate,
-				labels: ['MEETING']
-			},
-		};
-		rp(Object.assign({}, defaultOptions, issueOptions))
-			.then(function (response) {
-				console.log('Issue created successfully.');
-			})
-			.catch((err) => {
-				console.log('An error occurred while creating the Github issue.', err);
-			});
-	})
-	.catch(function (err) {
-		if (err.statusCode === 404) {
-			console.log('Unable to find ISSUE_TEMPLATE.md in that repository.');
-		} else {
-			console.log('Unknown error.');
-		}
-	});
-
-function getNextMeeting() {
-	return moment().recur().every("Tuesday").daysOfWeek()
-		.every(2).weeksOfMonthByDay().next(1);
-}
-
-function generateIssueTitle() {
-	const date = getNextMeeting();
-	return `${(date[0].format('MMM Do')).toUpperCase()} at 6:00pm`;
-}
+	console.log(msg);
+})
---
+++ webtask.js
@@ -0,0 +1,100 @@
+"use latest";
+
+const rp = require('request-promise-native');
+const moment = require('moment');
+require('moment-recur');
+
+const API_URL = 'https://api.github.com/';
+const GITHUB_RAW_URL = 'https://raw.githubusercontent.com/';
+const DAYS_OF_WEEK = ['SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY'];
+
+module.exports = function (context, cb) {
+	const {
+		o: owner,
+		r: repo,
+		d: dayOfWeek,
+		w: weekOfMonth
+	} = context.data;
+	const {
+		github_token,
+	} = context.secrets;
+
+	let msg;
+	let err;
+
+	if (!owner || !repo || !dayOfWeek || !weekOfMonth) {
+		err = 'Usage: curl <WEBTASK_URL>?o=REPO_OWNER_NAME&r=REPO_NAME&d=DAY_OF_WEEK&w=WEEK_OF_MONTH';
+		return cb(err);
+	}
+
+	if (!github_token) {
+		err= 'Github token not found in secrets. Did you set up the task correctly?'
+		return cb(err);
+	}
+
+	if (!isDayValid(dayOfWeek)) {
+		err = `Invalid day. Please use one of following: ${DAYS_OF_WEEK.join(', ')}`;
+		return cb(err);
+	}
+
+	const defaultOptions = {
+		qs: {
+			access_token: github_token
+		},
+		headers: {
+			'User-Agent': 'Github Issue Webtask'
+		},
+		json: true
+	};
+
+	const templateOptions = {
+		method: 'GET',
+		uri: `${GITHUB_RAW_URL}${owner}/${repo}/master/ISSUE_TEMPLATE.md`,
+	};
+	rp(Object.assign({}, defaultOptions, templateOptions))
+		.then((issueTemplate) => {
+			const title = generateIssueTitle(dayOfWeek, weekOfMonth);
+			const issueOptions = {
+				method: 'POST',
+				uri: `${API_URL}repos/${owner}/${repo}/issues`,
+				body: {
+					title,
+					body: issueTemplate,
+					labels: ['MEETING']
+				},
+			};
+			rp(Object.assign({}, defaultOptions, issueOptions))
+				.then((response) => {
+					msg = 'Issue created successfully.';
+					return cb(null, msg);
+				})
+				.catch((err) => {
+					err = 'An error occurred while creating the Github issue.';
+					return cb(err);
+				});
+		})
+		.catch(function (err) {
+			if (err.statusCode === 404) {
+				err = 'Unable to find ISSUE_TEMPLATE.md in that repository.';
+			} else {
+				err = 'Unknown error.';
+			}
+			return cb(err);
+		});
+};
+
+function getNextMeeting(dayOfWeek, weekOfMonth) {
+	const date = moment();
+	const firstOfNextMonth = date.month(date.month() + 1).date(1);
+	return moment(firstOfNextMonth).recur().every(dayOfWeek).daysOfWeek()
+		.every(weekOfMonth - 1).weeksOfMonthByDay().next(1);
+}
+
+function generateIssueTitle(dayOfWeek, weekOfMonth) {
+	const date = getNextMeeting(dayOfWeek, weekOfMonth);
+	return `${(date[0].format('MMM')).toUpperCase()} ${date[0].format('Do')} at 6:00pm`;
+}
+
+function isDayValid(dayOfWeek = '') {
+	return DAYS_OF_WEEK.findIndex((day) => day === dayOfWeek.toUpperCase()) >= 0;
+}
